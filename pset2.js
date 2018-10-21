fcab2fbce8ca5e9d841f356a7ff4332f80d831fc
Erika Perez <erikaperezvalle@pursuit.org>
2018-10-21 11:47:16 -0400

    pset2 completed

--- pset2.js
+++ pset2.js
@@ -5,6 +5,19 @@
     @desc - takes all engative numbers and makes positive
     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
 */
+console.log('--------problem1------');
+
+const removeNegatives = (arr)=>{
+    let positiveArr =[];
+    for (let i=0;i< arr.length;i++){
+        if (arr[i] < 0){
+        arr[i] = arr[i]*-1
+        }
+        positiveArr.push(arr[i])
+    }
+return positiveArr
+}
+console.log(removeNegatives([-3,-2,1,2,3,-4])); // [3,2,1,2,3,4]
 
 /*
     @func findVal
@@ -19,6 +32,21 @@
         findVal([1,2,3,4], 4); // 3
         findVal([1,2,3,4], 9); // -1
 */
+console.log('--------problem2------');
+
+const findVal = (arr,val)=>{
+    let i = [0]
+    for (let i=0;i < arr.length;i++){
+        if (arr[i]===val){
+            return i
+        }     
+    }
+ return '-1'
+}
+
+console.log(findVal([1,2,3,4], 1)); // 0
+console.log(findVal([1,2,3,4], 4)); // 3
+console.log(findVal([1,2,3,4], 9)); // -1
 
 /*
     @func removeOdds
@@ -31,6 +59,27 @@
         removeOdds([1,2,3,4]); // [2,4]
         removeOdds([1,"2",3,4]); // [4]
 */
+console.log('--------problem3------');
+
+const removeOdds = (arr)=>{
+    let noOdds = []
+    let i = [0]
+
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i]===[typeof'string']){
+            arr.pop(i)
+        }
+          
+     if (arr[i] % 2 ===0){
+        noOdds.push(arr[i])
+    }   
+   
+    }
+    return noOdds
+}
+
+console.log(removeOdds([1,2,3,4])); // [2,4]
+console.log(removeOdds([1,"2",3,4])); // [4]
 
 /*
     @func addSquares
@@ -40,7 +89,17 @@
     @example - 
         addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
 */
+console.log('--------problem4------');
 
+const addSquares = (arr)=>{
+    let newArr = [...arr]
+    //let newArr =[]
+    for (let i=0; i< arr.length; i++){
+        newArr.push(arr[i]*arr[i])
+    }
+    return newArr
+}
+console.log(addSquares([1,2,3,4])); // [1,2,3,4,1,4,9,16]
 /*
     @func doubleify
     @param {array} arr
@@ -48,8 +107,17 @@
     @desc - take each value of array, add same value right after
     @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
 */
+console.log('--------problem5------');
 
-
+const doubleify =(arr)=>{
+    let newArr2 = []
+    for (let i=0; i< arr.length; i++){
+        newArr2.push(arr[i]) && newArr2.push(arr[i])
+       
+    }
+    return newArr2
+}
+console.log(doubleify([1,2,3,4])); // [1,1,2,2,3,3,4,4]
 /*
     @func findMax
     @param {array}
@@ -57,3 +125,13 @@
     @desc - find largest number in array
     @example - findMax([1,2,3,99,4]); // 99 
 */
+console.log('--------problem6------');
+
+const findMax = (arr)=>{
+    
+    for (let i=0;i<arr.length;i++){
+        return (Math.max(...arr))
+   
+    }   
+}
+console.log(findMax([1,2,3,99,4])); // 99 )
